256315fbbe494366b44e3e6c0a6665bc7f976209
hdjr1 <143888752+hdjr1@users.noreply.github.com>
2024-09-07 08:08:23 +0800

    Add files via upload

---
+++ GRP4_rodriguezhadje_ex2.py
@@ -0,0 +1,16 @@
+"""
+Rodriguez Hadje B.
+BSCS3F
+
+Exercise 2
+
+#3 Creating int,float and complex variable then check the type using the Python Code
+
+"""
+int = 10
+float = 3.14
+complex = 2 + 3j
+
+print("Integer value : ", int)
+print("Float value : ", float)
+print("Complex value : ", complex)
---
+++ grp1_rodriguez_cselec2.py
@@ -0,0 +1,31 @@
+#My first simple script
+
+print ("Hello, Im Hadje Rodriguez BSCS 3F")
+
+age = 22
+name = "Hadje"
+gender = "Male"
+
+print("")
+
+print ("Name:",name)
+print ("Age:",age)
+print ("Gender:",gender)
+
+print("")
+rafael = "Rafael"
+mark = "Mark Angehelo"
+rochelle = "Rochelle"
+cylen = "Cylen"
+
+print (rafael)
+print (mark)
+print (rochelle)
+print (cylen)
+
+
+ 
+print("")
+ 
+age,edad,num = 21 ,21, 21
+print(age,edad,num)
